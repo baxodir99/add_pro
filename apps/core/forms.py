cdfad4ac565ce556711ea0ab27fc2ec64154d0f2
Bahodir Amondullayev <bahodiramondullayev@gmail.com>
2023-01-11 12:59:50 +0500

    update

--- apps/core/forms.py
+++ apps/core/forms.py
@@ -10,28 +10,17 @@ class BusinessForm(forms.ModelForm):
 
 
 class OutcomeForm(forms.ModelForm):
-    salary = forms.IntegerField(required=True)
-    marketing = forms.IntegerField(required=True)
+    salary = forms.IntegerField(required=True, min_value=0)
+    marketing = forms.IntegerField(required=True, min_value=0)
 
     class Meta:
         model = Outcome
         exclude = ("business", "month",)
-    
-        # def __init__ (self, salary, marketing, *args, **kwargs):
-        #     self.salary = salary
-        #     self.marketing = marketing
-        #     super (OutcomeForm, self).__init__ (*args, **kwargs)
-
 
 class IncomeForm(forms.ModelForm):
-    income = forms.IntegerField(required=True)
-    customer_count = forms.IntegerField(required=True)
+    income = forms.IntegerField(required=True, min_value=0)
+    customer_count = forms.IntegerField(required=True, min_value=0)
 
     class Meta:
         model = Income
         exclude = ("business", "month",)
-
-        # def __init__ (self, income, customer_count, *args, **kwargs):
-        #     self.income = income
-        #     self.customer_count = customer_count
-        #     super (IncomeForm, self).__init__ (*args, **kwargs)
